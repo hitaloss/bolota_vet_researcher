2dbf5c9b4ec78fd5da79abeacaf49480692af3e3
hitalo <hitalo.2014.hs@gmail.com>
2025-08-04 19:35:31 -0300

    fix: adjusted comma from removed dependency

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,4 +1,4 @@
-import { compare } from "bcrypt";
+import { compare } from "bcryptjs";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
@@ -20,34 +20,18 @@ async function sessionService({
   username,
   password,
 }: ISessionCreate): Promise<string> {
-  console.log(
-    `[DEBUG] Iniciando tentativa de login para o usuário: ${username}`
-  );
-
   try {
     const queryText = 'SELECT * FROM "User" WHERE username = $1';
     const result = await pool.query<IUser>(queryText, [username]);
     if (result.rowCount === 0) {
-      console.error(
-        `[DEBUG] Usuário '${username}' NÃO encontrado no banco de dados.`
-      );
       throw new AppError(403, "Invalid username or password.");
     }
     const user = result.rows[0];
-    console.log(`[DEBUG] Usuário '${username}' encontrado. ID: ${user.id}`);
-
-    console.log(`[DEBUG] Hash armazenado no banco: ${user.password}`);
     const passwordMatch = await compare(password, user.password);
     if (!passwordMatch) {
-      console.log(
-        `[DEBUG] Resultado da comparação de senha (bcrypt): ${passwordMatch}`
-      );
       throw new AppError(403, "Invalid username or password.");
     }
 
-    console.log(
-      `[DEBUG] Autenticação bem-sucedida para '${username}'. Gerando token...`
-    );
     const token = jwt.sign(
       { username: user.username },
       process.env.SECRET_KEY as string,
