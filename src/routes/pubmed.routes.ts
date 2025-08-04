1bf4669d2549981a589e4d38548cd0c2e7bcd76a
hitalo <hitalo.2014.hs@gmail.com>
2025-08-04 18:59:49 -0300

    feat: implemented authentication requirement

--- src/routes/pubmed.routes.ts
+++ src/routes/pubmed.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import searchPubmedHandlerController from "../controllers/pubmed.controller";
+import authMiddleware from "../middlewares/auth.middleware";
 
 const router = Router();
 
 export function pubmedRoutes() {
-  router.get("", searchPubmedHandlerController);
+  router.get("", authMiddleware, searchPubmedHandlerController);
   return router;
 }
 
---
+++ src/services/session.service.ts
@@ -0,0 +1,51 @@
+import { compare } from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+import { AppError } from "../errors/appError";
+import { pool } from "../database";
+
+export interface ISessionCreate {
+  username: string;
+  password: string;
+}
+
+interface IUser {
+  id: string;
+  username: string;
+  password: string;
+}
+
+async function sessionService({
+  username,
+  password,
+}: ISessionCreate): Promise<string> {
+  try {
+    const queryText = 'SELECT * FROM "User" WHERE username = $1';
+    const result = await pool.query<IUser>(queryText, [username]);
+    if (result.rowCount === 0) {
+      throw new AppError(403, "Invalid username or password.");
+    }
+    const user = result.rows[0];
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new AppError(403, "Invalid username or password.");
+    }
+
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.SECRET_KEY as string,
+      {
+        expiresIn: "1d",
+        subject: user.id.toString(),
+      }
+    );
+    return token;
+  } catch (error) {
+    if (error instanceof AppError) throw error;
+    console.error("Error on session service", error);
+    throw new AppError(500, "Internal server error.");
+  }
+}
+
+export default sessionService;
